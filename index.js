6e7105638f9b80782522e0c7cf900cf3a11f7a70
Kamrul Islam <hellodeveloper1993@gmail.com>
2025-07-14 21:52:13 +0600

    modified url-2

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", "0.0.0.0", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`);
 });
